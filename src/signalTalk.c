9931774677fd02d16f35c7ce74b12dc941d6e2f5
Fernando Dodino <fernando.dodino@gmail.com>
2013-07-24 13:16:31 +0000

    Versión inicial

---
+++ src/signalTalk.c
@@ -0,0 +1,88 @@
+/*
+ * signalTalk.c
+ *
+ *  Created on: 09/07/2013
+ *      Author: Fernando
+ */
+/*
+ * sig_talk.c
+ *
+ *  Created on: 09/07/2013
+ *      Author: Internet
+ */
+/* sig_talk.c --- Example of how 2 processes can talk */
+/* to each other using kill() and signal() */
+/* We will fork() 2 process and let the parent send a few */
+/* signals to it`s child  */
+
+/* cc sig_talk.c -o sig_talk  */
+
+//para Linux
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+// Para Windows
+//#include <sys/types.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <process.h>
+//#include <signal.h>
+//#include <windows.h>
+
+void sighup(); /* routines child will call upon sigtrap */
+void sigint();
+void sigquit();
+
+int main(void) {
+	int pid;
+
+	/* Creamos un proceso hijo */
+	if ((pid = fork()) < 0) {
+		perror("fork");
+		exit(1);
+	}
+
+	if (pid == 0) {
+		/* Proceso hijo */
+		/* Asociamos funciones para responder a cada signal */
+		signal(SIGHUP, sighup);
+		signal(SIGINT, sigint);
+		signal(SIGBUS, sigquit);
+		for (;;)
+			; /* loop infinito */
+	} else /* Proceso padre */{
+		/* pid mantiene el identificador del proceso hijo */
+		sleep(20);
+		printf("\nPARENT: sending SIGHUP\n\n");
+		kill(pid, SIGHUP);
+		sleep(10);
+		printf("\nPARENT: sending SIGINT\n\n");
+		kill(pid, SIGINT);
+		sleep(10);
+		printf("\nPARENT: sending SIGBUS\n\n");
+		printf("CHILD: %d\n", pid);
+		sleep(2);
+		kill(pid, SIGBUS);
+		sleep(10);
+	}
+	return 0;
+
+}
+
+void sighup() {
+	printf("CHILD: I have received a SIGHUP\n");
+	signal(SIGHUP, sighup); /* reset signal */
+}
+
+void sigint() {
+	printf("CHILD: I have received a SIGINT\n");
+	signal(SIGINT, sigint); /* reset signal */
+}
+
+void sigquit() {
+	printf("My DADDY has Killed me!!!\n");
+	exit(0);
+}
